73285edbb75a46a58448befb19453c83dfc3ce18
Sandeep <sandeep_kumbhar@mazakindia.com>
2018-05-28 08:38:26 +0530

    constructor and instance variable to store active clinet connection

--- src/app/Chat.php
+++ src/app/Chat.php
@@ -7,4 +7,13 @@ use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
 
+  protected $clients;
+
+  public function __construct() {
+    $this->clients = new \SplObjectStorage;
+    if($this->clients)
+      echo "Congratulations! the server is now running\n";
+    else
+      echo "Server is not running\n";
+  }
 }
