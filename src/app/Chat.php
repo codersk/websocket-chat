4618f245d514205f17c1825b0536cfc0fed7d465
Sandeep <sandeep_kumbhar@mazakindia.com>
2018-05-28 08:43:37 +0530

    onopen, operror, onmessage & onclose events handled in respective methods

--- src/app/Chat.php
+++ src/app/Chat.php
@@ -16,4 +16,35 @@ class Chat implements MessageComponentInterface {
     else
       echo "Server is not running\n";
   }
+
+  public function onOpen(ConnectionInterface $conn) {
+    // Store the new connection to send messages
+    $this->clients->attach($conn);
+    echo "New connection! ({$conn->resourceId})\n";
+  }
+
+  public function onMessage(ConnectionInterface $from, $msg) {
+    $numRecv = count($this->clients) - 1;
+    echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+      , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+    foreach ($this->clients as $client) {
+      // Send msg to every connected client except to the sender
+      if ($from !== $client) {
+        $client->send($msg);
+      }
+    }
+  }
+
+  public function onClose(ConnectionInterface $conn) {
+    // Remove closed connections
+    $this->clients->detach($conn);
+    echo "Connection {$conn->resourceId} has disconnected\n";
+  }
+
+  public function onError(ConnectionInterface $conn, \Exception $e) {
+    echo "An error has occurred: {$e->getMessage()}\n";
+    $conn->close();
+  }
+
 }
